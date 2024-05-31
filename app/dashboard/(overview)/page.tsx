9d6377f3c8f9ff735374a310b4cb03b1fe9006a3
orangegoo <1059078775@qq.com>
2024-06-01 00:49:30 +0800

    feat: adding metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,6 +4,11 @@ import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import { lusitana } from "../../ui/fonts";
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
 
 export default async function Page() {
   return (
